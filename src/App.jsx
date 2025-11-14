5fe033c5502daec6d1f3debef8e8528062e463a6
kisslex2013 <kisslex2013@example.com>
2025-11-14 14:50:53 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -602,7 +602,7 @@ function App() {
         {/* Версия приложения - более заметная надпись */}
         <div className="mt-4 mb-2 px-2 text-center">
           <p className="text-xs text-gray-400 dark:text-gray-500">
-            Time Tracker Dashboard v1.2.3 build_14.41_14.11.25</p>
+            Time Tracker Dashboard v1.2.3 build_14.50_14.11.25</p>
         </div>
 
         <FloatingPanel />
