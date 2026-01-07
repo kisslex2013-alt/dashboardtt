fef942b0a685387b1bcadb62c8e4729d605a3417
kisslex2013 <kisslex2013@example.com>
2026-01-07 16:10:43 +0300

    chore: add docs/ to .gitignore - documentation stored locally only

--- docs/prototypes/ЯНГ/Deploy/copy-files.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo Copying files...
-copy /Y "..\tasks-page.html" "tasks-page.html"
-copy /Y "..\accruals-page.html" "accruals-page.html"
-echo.
-echo Done! All files copied.
-pause
-
